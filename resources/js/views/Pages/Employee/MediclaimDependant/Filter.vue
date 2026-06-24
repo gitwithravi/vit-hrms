14500e04f8a990ca1d8f0e71ef93948734491632
Ravi Kumar Singh <ravi007virus@gmail.com>
2026-06-24 10:26:05 +0530

    Add employee filter to mediclaim dependants

--- resources/js/views/Pages/Employee/MediclaimDependant/Filter.vue
+++ resources/js/views/Pages/Employee/MediclaimDependant/Filter.vue
@@ -1,6 +1,40 @@
 <template>
-    <FilterForm :init-form="initForm" :form="form" @hide="emit('hide')">
+    <FilterForm
+        :init-form="initForm"
+        :form="form"
+        :multiple="['employees']"
+        @hide="emit('hide')"
+    >
         <div class="grid grid-cols-3 gap-6">
+            <div class="col-span-3 sm:col-span-1">
+                <BaseSelectSearch
+                    multiple
+                    v-if="fetchData.isLoaded"
+                    name="employees"
+                    :label="
+                        $trans('global.select', {
+                            attribute: $trans('employee.employee'),
+                        })
+                    "
+                    v-model="form.employees"
+                    value-prop="uuid"
+                    :init-search="fetchData.employees"
+                    search-key="name"
+                    search-action="employee/list"
+                >
+                    <template #selectedOption="slotProps">
+                        {{ slotProps.value.name }} ({{
+                            slotProps.value.codeNumber
+                        }})
+                    </template>
+
+                    <template #listOption="slotProps">
+                        {{ slotProps.option.name }} ({{
+                            slotProps.option.codeNumber
+                        }})
+                    </template>
+                </BaseSelectSearch>
+            </div>
             <div class="col-span-3 sm:col-span-1">
                 <BaseInput
                     type="text"
@@ -44,7 +78,8 @@ export default {
 </script>
 
 <script setup>
-import { reactive } from "vue"
+import { reactive, onMounted } from "vue"
+import { useRoute } from "vue-router"
 
 defineProps({
     preRequisites: {
@@ -55,9 +90,12 @@ defineProps({
     },
 })
 
+const route = useRoute()
+
 const emit = defineEmits(["hide"])
 
 const initForm = {
+    employees: [],
     name: "",
     relationship: "",
     gender: "",
@@ -65,4 +103,16 @@ const initForm = {
 }
 
 const form = reactive({ ...initForm })
+
+const fetchData = reactive({
+    employees: [],
+    isLoaded: route.query.employees ? false : true,
+})
+
+onMounted(async () => {
+    fetchData.employees = route.query.employees
+        ? route.query.employees.split(",")
+        : []
+    fetchData.isLoaded = true
+})
 </script>
